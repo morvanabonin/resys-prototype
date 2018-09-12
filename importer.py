ba8a0580be1f75d1b0937d39e92a925f1d02b114
Morvana Bonin <morvanabonin@gmail.com>
2018-09-11 22:55:38 -0300

    WIP

--- importer.py
+++ importer.py
@@ -1,6 +1,12 @@
 # Importer movielens dataset for Neo4J database
 
-from neo4j import GraphDatabase
+# from neo4j import GraphDatabase
+from neo4j.v1 import GraphDatabase
 
-db = GraphDatabase("http://localhost:7474", username="neo4j", password="neo4j")
+driver = GraphDatabase.driver("bolt://172.20.0.2:7687", auth=("neo4j", "neo4j"), encrypted=false)
 
+# db = GraphDatabase("http://localhost:7474", username="neo4j", password="neo4j")
+# print_r(db)
+
+# db = GraphDatabase("bolt://172.20.0.2:7687", username="neo4j", password="neo4j")
+# print_r(db)
