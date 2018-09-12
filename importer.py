0ddf19fa6e8cecdbea1c9d503a5f105597670c0f
Morvana Bonin <morvana.bonin@kinghost.com.br>
2018-09-12 17:55:23 -0300

    Funcionou! \o/

--- importer.py
+++ importer.py
@@ -3,8 +3,9 @@
 # from neo4j import GraphDatabase
 from neo4j.v1 import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://172.20.0.2:7687", auth=("neo4j", "neo4j"), encrypted=false)
-
+# driver = GraphDatabase.driver("bolt://172.20.0.2:7687", auth=("neo4j", "neo4j"), encrypted=false)
+driver = GraphDatabase.driver("bolt://172.19.0.2:7687", auth=("neo4j", "201125"))
+print(driver)
 # db = GraphDatabase("http://localhost:7474", username="neo4j", password="neo4j")
 # print_r(db)
 
