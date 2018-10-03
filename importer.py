83b647f1a7806fd5abb4c73be375fcb5029c7240
Morvana Bonin <morvanabonin@gmail.com>
2018-10-03 11:57:29 -0300

    WIP: lógica para parsear os arquivos e gerar um com dados em conjunto.

--- importer.py
+++ importer.py
@@ -14,12 +14,71 @@ try:
 except Exception as e:
     logger.error("Houve erro ao efetuar a conexão com o banco. Erro: {0}".format(e))
 
-#leitura do csv
+
+# leitura do csv
 try:
-    with open('ml-latest/movies.csv', 'r+') as csvmovies:
-        movies = csv.reader(csvmovies, delimiter=',', quotechar='|')
-        for movie in movies:
-            print(movie[0])
+    # with open('ml-latest/movies.csv', 'r+') as csvmovies:
+    #     movies = csv.reader(csvmovies, delimiter=',', quotechar='|')
+    #     for movie in movies:
+
+    with open('ml-latest/movies.csv', 'r') as movieFile:
+        reader = csv.reader(movieFile)
+        movies = list(reader)
+
+    with open('ml-latest/users.csv', 'r') as usersFile:
+        reader = csv.reader(usersFile)
+        users = list(reader)
+
+    with open('ml-latest/ratings.csv', 'r') as ratingsFile:
+        reader = csv.reader(ratingsFile)
+        ratings = list(reader)
+
+    # for index, movie in enumerate(movies):
+    for movie in movies:
+        indexMovie = movie[0]
+        nameMovie = movie[1]
+        genres = movie[2]
+        for user in users:
+            indexUser = user[0]
+            nameUser = user[1]
+            surname = user[2]
+            for rating in ratings:
+                userId = rating[0]
+                movieId = rating[1]
+                rating = rating[2]
+
+                if(indexMovie == movieId and indexUser == userId):
+                    name = nameUser +' '+surname
+                    relation = [name, nameMovie, genres, rating]
+                    with open('ml-latest/relations.csv', 'a', newline='', encoding='utf-8') as writeFile:
+                        writer = csv.writer(writeFile)
+                        writer.writerow(relation)
+                    writeFile.close()
+    movieFile.close()
+    usersFile.close()
+    ratingsFile.close()  
+    
 except Exception as e:
     logger.error("Houve erro ao ler o arquivo de CSV. Erro: {0}".format(e))
 
+def testeCreate():
+    # abrindo uma sessão no Neo4J
+    dbg = driver.session()
+    # Match para retorno de dados (tipo um select)
+    ret = dbg.run("CREATE (database:Database {name:'Neo4j'})-[r:SAYS]->(message:Message {name:'Hello World!'}) RETURN database, message, r")
+    # for do resultado
+    for r in ret:
+        print(r)
+    # fechando sessão
+    dbg = driver.close()
+
+def testeConexãoMatch():
+    # abrindo uma sessão no Neo4J
+    dbg = driver.session()
+    # Match para retorno de dados (tipo um select)
+    ret = dbg.run("MATCH (a)-[:ACTED_IN]->(m)<-[:DIRECTED]-(d) RETURN a,m,d LIMIT 10")
+    # for do resultado
+    for r in ret:
+        print(r)
+    # fechando sessão
+    dbg = driver.close()
